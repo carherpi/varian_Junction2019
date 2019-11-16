dc31f5fcb2b64e38db799b045fbbb7c338a62d88
Carlos Herrera <carloshpiedra@gmail.com>
2019-11-16 14:39:37 +0200

    new font-family

--- varian/src/app/components/dashboard/checklist/checklist.component.ts
+++ varian/src/app/components/dashboard/checklist/checklist.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChecklistComponent implements OnInit {
 
-toCheck = ['awdsef','bsdfs','sedfac','dwaefraw']
+toCheck = ['asdfs','bsdfs','sedfac','dwaefraw']
 
   constructor() { }
 
