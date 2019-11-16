8581091e61f4750c9bff0572b78cadafc23b68c6
Victor Callejas <victorcf999@gmail.com>
2019-11-16 04:09:37 +0100

    componente histograma

---
+++ varian/src/app/components/dashboard/histograma/histograma.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-histograma',
+  templateUrl: './histograma.component.html',
+  styleUrls: ['./histograma.component.scss']
+})
+export class HistogramaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
